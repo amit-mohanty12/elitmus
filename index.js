58b2c168c7cfe0a3c84251ee49f76575dda4117b
Saurjyaman Nayak <sauryaman123@gmail.com>
2023-02-24 17:49:44 +0530

    camera function added

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-//  const getJokes=async()=>
-//  {
-//      try{
-//          const res= await fetch("https://api.chucknorris.io/jokes/random");
-//          const data = await res.json();
-//          const test= document.querySelector("#test");
-//          test.innerHTML = data.value;
-// }catch(error){}
-//  };
 
-//  window.addEventListener("load",()=>{
-//      getJokes();
-
-//  });
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-database.js"></script>
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// <script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-database.js"></script>
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC86I0TH3GubhBeD17F2lSiY2wtbKuhB8E",
-  authDomain: "e-litmus-f57af.firebaseapp.com",
-  databaseURL: "https://e-litmus-f57af-default-rtdb.firebaseio.com",
-  projectId: "e-litmus-f57af",
-  storageBucket: "e-litmus-f57af.appspot.com",
-  messagingSenderId: "741603547483",
-  appId: "1:741603547483:web:2aac20b934f8d47f50627e",
-  measurementId: "G-PBWX0E1PWD"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 console.log("hello");
 function callvalue(event){
 
@@ -46,9 +18,30 @@ function callvalue(event){
     var name = document.getElementById("fname").value;
     var email= document.getElementById("email").value;
     var Regno=document.getElementById("number").value;
+    var code=document.getElementById("invitation").value;
+    var d= new Date();
+    var format=d.getDate()+" / "+(d.getMonth()+1)+" / "+d.getFullYear()+" "+(d.getHours())+" : "+(d.getMinutes()+1)+" : "+(d.getSeconds()+1)+" GMT +0530 (Indian Standard Time)";
+
+    var tgref=firebase.database().ref("userinfo/");
+    var data={
+        "name":name,
+        "email":email,
+        "Registration number":Regno,
+        "Invitation code": code,
+        "time": format,
+    }
+    tgref.push(data).then((res)=>{
+    console.log(res.key)
+    localStorage.setItem("key",res.key)
+        console.log("Data sent successfully!");
+        window.open("camera/camera.html", "_self")             
+    });    
+
     console.log(name);
 }
 const form =document.getElementById("user_form");
 form.addEventListener("submit",callvalue,true);
+
+
 
 
